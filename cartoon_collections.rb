a3efd1b953f44634559c71e67f0443005946eed9
Saung Oo Myat <saung.myat@gmail.com>
2022-10-11 21:50:25 -0400

    commit tree d19a9c80af1d085b4c718995402e411f05ffb659

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,24 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
+    dwarves.each_with_index do |dwarf, index|
+      puts "#{index + 1}. #{dwarf}"
+    end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
+  planeteer_calls.map { |call| call.capitalize + '!' }
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(planeteer_calls)# code an argument here
   # Your code here
+  planeteer_calls.any? { |call| call.length > 4 }
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(cheeses)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheeses.find do |cheese|
+    cheese_types.include?(cheese)
+  end
 end
